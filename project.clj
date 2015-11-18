0016f13c2aa4e5010a119057f21b83409e1b59e1
Edoardo Negri <edne@gmx.com>
2015-11-18 08:19:51 +0100

    Affero GPL License

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject b3api "0.1.0-SNAPSHOT"
   :description "b3api"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/Politecnico-Open-unix-Labs/b3api"
+  :license {:name "GNU Affero General Public License"
+            :url "http://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [cheshire "5.5.0"]
